5ede79e4bcb496ac9ec73d066ba5c838d310beef
Daiane Silva <daiane.lourenco29@gmail.com>
2024-11-22 05:53:11 +0000

    Atualizações no banco de dados e scripts

--- models/conexao.php
+++ models/conexao.php
@@ -5,13 +5,10 @@ $password = "";      // A senha geralmente é vazia no XAMPP
 $dbname = "agendamento_consultas"; // Nome do banco de dados
 
 // Criando a conexão
-$conexao = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Checando a conexão
-if ($conexao->connect_error) {
-   
- die("Falha na conexão: " . $conexao->connect_error);
- echo "Conexão bem-sucedida!";
-
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
 }
 ?>
